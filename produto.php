ea55857b9f94ab05cfc81e6308dd3a2f00564c4a
ValeriaNiceria <valerianiceria@gmail.com>
2017-06-29 13:07:12 -0300

    Mobile First - Produtos

--- produto.php
+++ produto.php
@@ -1,13 +1,47 @@
+        <!--Inicio cabeçalho-->
         <?php
-            $cabecalho_css='<link rel="stylesheet" href="css/estilo.css">';
+            $cabecalho_css='<link rel="stylesheet" href="css/produto.css">';
             $cabecalho_title="Produto da Mirror Fashion";
             include("cabecalho.php");
         ?>
+        <!--Fim cabeçalho-->
 
-        <!--Aqui vai o miolo da página depois! -->
+        <!--Inicio - Formulário de Produtos-->
+       <div class="produto">
+            <h1>Camiseta Ecko Caveira Bad to The Bone</h1>
+            <p>Por apenas R$ 48,95</p>
 
+            <form>
+                <fieldset class="cores">
+                    <legend>
+                        Escolha a cor:
+                    </legend>
+
+                    <input type="radio" name="cor" value="verde" id="verde" checked>
+                    <label for="verde">
+                        <img src="img/produtos/foto2-verde.png" alt="Produto verde">
+                    </label>
+
+                    <input type="radio" name="cor" value="rosa" id="rosa">
+                    <label for="rosa">
+                        <img src="img/produtos/foto2-rosa.png" alt="Produto rosa">
+                    </label>
+
+                    <input type="radio" name="cor" value="azul" id="azul">
+                    <label for="azul">
+                        <img src="img/produtos/foto2-azul.png" alt="Produto azul">
+                    </label>
+                </fieldset>
+
+                <input type="submit" class="comprar" value="Comprar">
+            </form>
+
+       </div>
+
+       <!--Inicio rodapé-->
         <?php
             include("rodape.php");
         ?>
+        <!--Fim rodapé-->
     </body>
 </html>
