73e384ffc548aa2a93e273d104adfc1aabafd292
ValeriaNiceria <valerianiceria@gmail.com>
2017-07-27 17:06:48 -0300

    Grid Responsivo do Bootstrap

--- checkout.php
+++ checkout.php
@@ -20,87 +20,92 @@
     </div>
 
     <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <!--Adicionando a página checkout as informações de cor e tamanho escolhidas -->
-                <h2 class="panel-title">Sua compra</h2>
-            </div>
-            <div class="panel-body">
-                <img src="img/produtos/foto<?=$_POST['id']?>-<?=$_POST['cor']?>.png" class="img-thumbnail img-responsive">
-                <dl> <!--A lista é a 'DL'-->
-                    <dt>Produto</dt>
-                    <dd><?=$_POST['nome']?></dd>
+        <div class="row"> <!--Usar grids do Bootstrap para transformar nosso design em duas colunas em telas maiores-->
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <!--Adicionando a página checkout as informações de cor e tamanho escolhidas -->
+                        <h2 class="panel-title">Sua compra</h2>
+                    </div>
+                    <div class="panel-body">
+                        <img src="img/produtos/foto<?=$_POST['id']?>-<?=$_POST['cor']?>.png" class="img-thumbnail img-responsive hidden-xs"> <!--A classe hidden-xs: vai esconder o elemento  em resoluções menores de 768px-->
+                        <dl> <!--A lista é a 'DL'-->
+                            <dt>Produto</dt>
+                            <dd><?=$_POST['nome']?></dd>
 
-                    <dt>Cor</dt><!--Cada termo é representado por um 'DT'-->
-                    <dd><?=$_POST['cor']?></dd><!--Sua definição em um 'DD'-->
+                            <dt>Cor</dt><!--Cada termo é representado por um 'DT'-->
+                            <dd><?=$_POST['cor']?></dd><!--Sua definição em um 'DD'-->
 
-                    <dt>Tamanho</dt>
-                    <dd><?=$_POST['tamanho']?></dd>
+                            <dt>Tamanho</dt>
+                            <dd><?=$_POST['tamanho']?></dd>
 
-                    <dt>Preço</dt>
-                    <dd><?=$_POST['preco']?></dd>
-                </dl>
-            </div>
-        </div>
-
-        <form>
-            <fieldset>
-                <legend>Dados pessoais</legend>
-
-                <div class="form-group">
-                    <label for="nome">Nome completo</label>
-                    <input type="text" class="form-control" id="nome" name="nome" autofocus required>
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <div class="input-group">
-                        <span class="input-group-addon">@</span> <!--Validando o campo email - apenas usando o input type correto-->
-                        <input type="email" class="form-control" id="email" name="email" placeholder="email@exemplo.com">
+                            <dt>Preço</dt>
+                            <dd><?=$_POST['preco']?></dd>
+                        </dl>
                     </div>
                 </div>
+            </div>
+                <form class="col-sm-8">
+                    <div class="row">
+                        <fieldset class="col-md-6">
+                            <legend>Dados pessoais</legend>
 
-                <div class="form-group">
-                    <label for="cpf">CPF</label>
-                    <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00" required>
-                </div>
+                            <div class="form-group">
+                                <label for="nome">Nome completo</label>
+                                <input type="text" class="form-control" id="nome" name="nome" autofocus required>
+                            </div>
 
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="sim" name="spam" checked>
-                        Quero receber spam da Mirror Fashion
-                    </label>
-                </div>
-            </fieldset>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <div class="input-group">
+                                    <span class="input-group-addon">@</span> <!--Validando o campo email - apenas usando o input type correto-->
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="email@exemplo.com">
+                                </div>
+                            </div>
 
-            <fieldset>
-                <legend>Cartão de crédito</legend>
+                            <div class="form-group">
+                                <label for="cpf">CPF</label>
+                                <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00" required>
+                            </div>
 
-                <div class="form-group">
-                    <label for="numero-cartao">Número - CVV</label>
-                    <input type="text" class="form-control" id="numero-cartao" name="numero-cartao">
-                </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" value="sim" name="spam" checked>
+                                    Quero receber spam da Mirror Fashion
+                                </label>
+                            </div>
+                        </fieldset>
 
-                <div class="form-group">
-                    <label for="bandeira-cartao">Bandeira</label>
-                    <select name="bandeira-cartao" id="bandeira-cartao" class="form-control">
-                        <option value="master">MasterCard</option>
-                        <option value="visa">VISA</option>
-                        <option value="amex">American Express</option>
-                    </select>
-                </div>
+                        <fieldset class="col-md-6">
+                            <legend>Cartão de crédito</legend>
 
-                <div class="form-group">
-                    <label for="validade-cartao">Validade</label>
-                    <input type="month" class="form-control" id="validade-cartao" name="validade-cartao">
-                </div>
-            </fieldset>
+                            <div class="form-group">
+                                <label for="numero-cartao">Número - CVV</label>
+                                <input type="text" class="form-control" id="numero-cartao" name="numero-cartao">
+                            </div>
 
-            <button type="submit" class="btn btn-primary pull-right btn-lg">
-                <span class="glyphicon glyphicon-thumbs-up"></span> <!--Adicionar ícone do BOOTSTRAP-->
-                Confirmar Pedido
-            </button>
-        </form>
+                            <div class="form-group">
+                                <label for="bandeira-cartao">Bandeira</label>
+                                <select name="bandeira-cartao" id="bandeira-cartao" class="form-control">
+                                    <option value="master">MasterCard</option>
+                                    <option value="visa">VISA</option>
+                                    <option value="amex">American Express</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="validade-cartao">Validade</label>
+                                <input type="month" class="form-control" id="validade-cartao" name="validade-cartao">
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary pull-right btn-lg">
+                        <span class="glyphicon glyphicon-thumbs-up"></span> <!--Adicionar ícone do BOOTSTRAP-->
+                        Confirmar Pedido
+                    </button>
+                </form>
+        </div>
     </div>
 
 </body>
