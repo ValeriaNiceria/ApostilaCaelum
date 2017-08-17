c1ba96774845791d4ad8099746e531cca5b0b937
ValeriaNiceria <valerianiceria@gmail.com>
2017-08-17 16:03:41 -0300

    Fontes customizadas - Google Web Fonts

--- cabecalho.php
+++ cabecalho.php
@@ -1,46 +1,48 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title><?php print $cabecalho_title;?></title>
+<head>
+    <meta charset="UTF-8">
+    <title><?php print $cabecalho_title;?></title>
 
-        <link rel="stylesheet" href="css/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans|Bad+Script" rel="stylesheet"> <!--Importano a font do site do google-->
 
-        <!--Cada página incluir o arquivo css própio-->
-        <?php print @$cabecalho_css;?> <!--Utilizamos o '@' para tratar o erro, caso a variável não exista-->
+    <link rel="stylesheet" href="css/reset.css">
 
-        <!--Ajustar a media query para aplicar o css de adaptação a qualquer tamanho de tela menor que os 940px do desktop(ou seja, no máximo,939px)-->
-        <link rel="stylesheet" href="css/mobile.css" media="(max-width:939px)">
+    <!--Cada página incluir o arquivo css própio-->
+    <?php print @$cabecalho_css;?> <!--Utilizamos o '@' para tratar o erro, caso a variável não exista-->
 
-        <!--Adaptação para mobile-->
-        <meta name="viewport" content="width=device-width">
-    </head>
+    <!--Ajustar a media query para aplicar o css de adaptação a qualquer tamanho de tela menor que os 940px do desktop(ou seja, no máximo,939px)-->
+    <link rel="stylesheet" href="css/mobile.css" media="(max-width:939px)">
 
-    <body class="container">
-        <header>
+    <!--Adaptação para mobile-->
+    <meta name="viewport" content="width=device-width">
+</head>
 
-            <!--Suporte HTML5 no Internet Explorer Antigo-->
+<body class="container">
+    <header>
+
+        <!--Suporte HTML5 no Internet Explorer Antigo-->
                     <!--[if lt IE 9]>
                     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js">
                     </script>
                     <![endif]-->
-                <!--Fim código do suporte-->
+                    <!--Fim código do suporte-->
 
-            <h1>
-                <img src="img/logo.png" alt="Mirror Fashion">
-            </h1>
+                    <h1>
+                        <img src="img/logo.png" alt="Mirror Fashion">
+                    </h1>
 
-            <div class="sacola">
-                Nenhum item na sacola de compras
-            </div>
+                    <div class="sacola">
+                        Nenhum item na sacola de compras
+                    </div>
 
-            <nav class="menu-opcoes">
-                <ul>
-                    <li><a href="#">Sua Conta</a></li>
-                    <li><a href="#">Lista de desejos</a></li>
-                    <li><a href="#">Cartão Fidelidade</a></li>
-                    <li><a href="sobre.php">Sobre</a></li>
-                    <li><a href="#">Ajuda</a></li>
-                </ul>
-            </nav>
-        </header>
+                    <nav class="menu-opcoes">
+                        <ul>
+                            <li><a href="#">Sua Conta</a></li>
+                            <li><a href="#">Lista de desejos</a></li>
+                            <li><a href="#">Cartão Fidelidade</a></li>
+                            <li><a href="sobre.php">Sobre</a></li>
+                            <li><a href="#">Ajuda</a></li>
+                        </ul>
+                    </nav>
+                </header>
