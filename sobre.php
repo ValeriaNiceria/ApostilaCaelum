ad967c2e7c1ce04f01f0aa9255af76e93452677e
ValeriaNiceria <valerianiceria@gmail.com>
2017-08-17 15:42:09 -0300

    trabalhando com iframe

--- sobre.php
+++ sobre.php
@@ -1,20 +1,24 @@
 
-    <!--Inicio cabeçalho-->
-    <?php
-        $cabecalho_css='<link rel="stylesheet" href="css/sobre.css">';
-        $cabecalho_title="Sobre a Mirror Fashion";
-        include("cabecalho.php");
-    ?>
-    <!--Fim cabeçalho-->
+<!--Inicio cabeçalho-->
+<?php
+$cabecalho_css = '<link rel="stylesheet" href="css/sobre.css">';
+$cabecalho_title = "Sobre a Mirror Fashion";
+include "cabecalho.php";
+?>
+<!--Fim cabeçalho-->
 
-    <article>
-            <h1 id="titulo">A Mirror Fashion</h1>
+<article>
+    <h1 id="titulo">A Mirror Fashion</h1>
 
-            <p>A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo.
-            Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de
-            participação em 118 deles.</p>
+    <div class="video">
+        <iframe width="460" height="300" src="https://www.youtube.com/embed/Tb06abHE4hY" frameborder="0" allowfullscreen></iframe>
+    </div>
 
-            <p>Nosso centro de distribuição fica em <a target="_blank" href="https://www.google.com.br/maps/place/Jacarezinho+-+PR/@-23.1461904,-50.2545319,10z/data=!3m1!4b1!4m5!3m4!1s0x94c0264dfb650a67:0x5e73a1bcbcfd5760!8m2!3d-23.1580947!4d-49.9777136">Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que
+    <p>A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo.
+        Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de
+        participação em 118 deles.</p>
+
+        <p>Nosso centro de distribuição fica em <a target="_blank" href="https://www.google.com.br/maps/place/Jacarezinho+-+PR/@-23.1461904,-50.2545319,10z/data=!3m1!4b1!4m5!3m4!1s0x94c0264dfb650a67:0x5e73a1bcbcfd5760!8m2!3d-23.1580947!4d-49.9777136">Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que
             distribuem nossos produtos às casas do mundo todo. Nosso centro de distribuição:</p>
 
             <figure id="centro-distribuicao">
@@ -24,56 +28,57 @@
             </figure>
 
             <p>Compre suas roupas e acessórios na <strong>Mirror Fashion</strong>. Acesse  <a href="index.php">nossa loja</a> ou entre em contato
-            se tiver dúvidas. Conheça também nossa <a href="#historias">história</a> e nossos <a href="#diferenciais">diferenciais</a>.</p>
+                se tiver dúvidas. Conheça também nossa <a href="#historias">história</a> e nossos <a href="#diferenciais">diferenciais</a>.</p>
 
 
-            <h2 id="historias">História</h2>
+                <h2 id="historias">História</h2>
 
-            <figure id="familia-pelho">
-                <img src="img/familia-pelho.jpg" title="Família Pelho" alt="[Família Pelho]"/>
-                <figcaption>Família Pelho</figcaption>
-            </figure>
+                <figure id="familia-pelho">
+                    <img src="img/familia-pelho.jpg" title="Família Pelho" alt="[Família Pelho]"/>
+                    <figcaption>Família Pelho</figcaption>
+                </figure>
 
-            <p>A fundação em 1932 ocorreu no momento da descoberta econônica do interior do Paraná. Fundada há <?php print date('Y') - 1932; ?> anos. A
-            <em>família Pelho</em>, tradicional da região, investiu todas as suas economias nessa nova iniciativa,
-            revolucionária para a época. O fundador <em>Eduardo Simões Pelho</em>, dotado de particular visão
-            administrativa, guiou os negócios da empresa durante mais de 50 anos, muitos deles ao lado
-            de seu filho <em>E. S. Pelho Filho</em>, atual CEO. O nome da empresa é inspirado no nome da família.</p>
+                <p>A fundação em 1932 ocorreu no momento da descoberta econônica do interior do Paraná. Fundada há <?php print date('Y') - 1932;?> anos. A
+                    <em>família Pelho</em>, tradicional da região, investiu todas as suas economias nessa nova iniciativa,
+                    revolucionária para a época. O fundador <em>Eduardo Simões Pelho</em>, dotado de particular visão
+                    administrativa, guiou os negócios da empresa durante mais de 50 anos, muitos deles ao lado
+                    de seu filho <em>E. S. Pelho Filho</em>, atual CEO. O nome da empresa é inspirado no nome da família.</p>
 
-            <p>O crescimento da empresa foi praticamente instantâneo. Nos primeiros 5 anos, já atendia 18 países.
-            Bateu a marca de 100 países em apenas 15 anos de existência. Até hoje, já atendeu 740 milhões
-            de usuários diferentes, em bilhões de diferentes pedidos.</p>
+                    <p>O crescimento da empresa foi praticamente instantâneo. Nos primeiros 5 anos, já atendia 18 países.
+                        Bateu a marca de 100 países em apenas 15 anos de existência. Até hoje, já atendeu 740 milhões
+                        de usuários diferentes, em bilhões de diferentes pedidos.</p>
 
-            <p>O crescimento em número de funcionários é também assombroso. Hoje, é a maior empregadora do
-            Brasil, mas mesmo após apenas 5 anos de sua existência, já possuía 30 mil funcionários. Fora do
-            Brasil, há 240 mil funcionários, além dos 890 mil brasileiros nas instalações de Jacarezinho e
-            nos escritórios em todo país.</p>
+                        <p>O crescimento em número de funcionários é também assombroso. Hoje, é a maior empregadora do
+                            Brasil, mas mesmo após apenas 5 anos de sua existência, já possuía 30 mil funcionários. Fora do
+                            Brasil, há 240 mil funcionários, além dos 890 mil brasileiros nas instalações de Jacarezinho e
+                            nos escritórios em todo país.</p>
 
-            <p>Dada a importância econômica da empresa para o Brasil, a <em>família Pelho</em> já recebeu diversos prêmios,
-            homenagens e condecorações. Todos os presidentes do Brasil já visitaram as instalações da <strong>Mirror
-            Fashion</strong>, além de presidentes da União Européia, Ásia e o secretário-geral da ONU.</p>
-
-
-            <h2 id="diferenciais">Diferenciais</h2>
-            <ul>
-                <li> Menor preço do varejo, garantido</li>
-                <li>Se você achar uma loja mais barata, leva o produto de graça</li>
-                <li> Pague em reais, dólares, euros ou bitcoins</li>
-                <li> Todas as compras com frete grátis para o mundo todo</li>
-                <li> Maior comércio eletrônico de moda do mundo</li>
-                <li> Atendimento via telefone, email, chat, twitter, facebook, carta, fax e telegrama</li>
-                <li>Presente em 124 países</li>
-                <li> Mais de um milhão de funcionários em todo o mundo</li>
-            </ul>
-
-        </article>
-
-        <!--Inicio rodapé-->
-        <?php
-            include("rodape.php");
-        ?>
-        <!--Fim rodapé-->
+                            <p>Dada a importância econômica da empresa para o Brasil, a <em>família Pelho</em> já recebeu diversos prêmios,
+                                homenagens e condecorações. Todos os presidentes do Brasil já visitaram as instalações da <strong>Mirror
+                                Fashion</strong>, além de presidentes da União Européia, Ásia e o secretário-geral da ONU.</p>
 
 
-</body>
-</html>
+                                <h2 id="diferenciais">Diferenciais</h2>
+                                <ul>
+                                    <li> Menor preço do varejo, garantido</li>
+                                    <li>Se você achar uma loja mais barata, leva o produto de graça</li>
+                                    <li> Pague em reais, dólares, euros ou bitcoins</li>
+                                    <li> Todas as compras com frete grátis para o mundo todo</li>
+                                    <li> Maior comércio eletrônico de moda do mundo</li>
+                                    <li> Atendimento via telefone, email, chat, twitter, facebook, carta, fax e telegrama</li>
+                                    <li>Presente em 124 países</li>
+                                    <li> Mais de um milhão de funcionários em todo o mundo</li>
+                                </ul>
+
+                            </article>
+
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14625.7973761777!2d-46.6323547!3d-23.5882126!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xab35da2f5ca62674!2sCaelum+-+Ensino+e+Inova%C3%A7%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1502994474877" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <!--Inicio rodapé-->
+                            <?php
+include "rodape.php";
+?>
+                            <!--Fim rodapé-->
+
+
+                        </body>
+                        </html>
